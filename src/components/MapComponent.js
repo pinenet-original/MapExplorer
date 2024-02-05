5f866365b70674997f0e8e30382c078f64bb0ffe
mdainys <marius.dainys@pinenet.io>
2024-02-05 16:27:51 +0200

    nav botom1

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -79,9 +79,12 @@ export const MapComponent = ({ selectedRoute, stopRoute, setShowMap }) => {
           duration: 2000,
           padding: { top: 400, bottom: 0, left: 0, right: 0 },
         });
+
         return {
+          ...prevViewport,
           latitude: currentLocation.latitude,
           longitude: currentLocation.longitude,
+          zoom: 20,
         };
       } else {
         mapRef.current.easeTo({
